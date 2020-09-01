9fb5fb9fdc24e1414eb8f2269befc29e76c13cee
José Maurício Guimarães França <josemauriciogf@gmail.com>
2020-09-01 14:46:06 -0300

    Criação da workspace - exercicio03

---
+++ Exercico03/src/code/AlteracaoAleatoria.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package code;
+
+/**
+ * @author josem
+ *
+ */
+public class AlteracaoAleatoria {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

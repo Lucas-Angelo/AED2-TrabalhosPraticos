d1f3407294dc089826a2248acf3ff8492afaf56a
Guilherme <guilherme.gabriel.532@gmail.com>
2020-10-14 17:59:01 -0300

    Add Exercicio06

---
+++ TP-2/ForVerde/Exercicio06/App.java
@@ -0,0 +1,434 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.EOFException;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class App {
+
+	public static void main(String[] args) throws IOException {
+		int comparacoes; // Para o log
+		int movimentacoes; // Para o log
+
+		long inicio = System.currentTimeMillis(); // Para o log
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String entrada = new String();
+
+		ArquivoTextoLeitura leitura = new ArquivoTextoLeitura();
+
+		int qtdJogadores = qtdLinhas(leitura); // Cada linha � um jogador, procurar o n�mero de linhas pra facilitar
+		int i = 0; // Utilizado para selecionar posicoes do vetor com alguns jogadores, e no final
+					// saber quantos jogadores tem no vetor
+		int qtdJogadoresVetor;
+
+		Jogador[] players = preencherVetorJogador(leitura, qtdJogadores);
+		Jogador[] vetor = new Jogador[qtdJogadores];
+
+		do {
+			entrada = in.readLine();
+
+			if (!(entrada.equals("FIM"))) {
+
+				vetor[i] = new Jogador();
+				vetor[i] = players[Integer.parseInt(entrada)].clone(); // Clona os dados do jogador com ID informado
+				i++;
+
+			}
+		} while (!(entrada.equals("FIM")));
+
+		qtdJogadoresVetor = i; // Agora ja se sabe quantos jogadores existem nesse vetor
+
+		Merge ordenar = new Merge(); // Objeto da classe que possui o metodo de ordenar
+
+		vetor = ordenar.sort(vetor, qtdJogadoresVetor); // Vetor recebe de jogadores recebe ele mesmo ordenado por nome
+
+		for (i = 0; i < qtdJogadoresVetor; i++) {
+			vetor[i].imprimir(); // Imprime os dados do vetor ordenado por nome
+		}
+
+		comparacoes = ordenar.getComparacoes();
+		movimentacoes = ordenar.getMovimentacoes();
+
+		long fim = System.currentTimeMillis();
+		gerarLog(inicio, fim, comparacoes, movimentacoes);
+
+	}
+
+	public static void gerarLog(long inicio, long fim, int comparacoes, int movimentacoes) {
+		long mili = fim - inicio;
+
+		ArquivoTextoEscrita escrita = new ArquivoTextoEscrita();
+		String log = new String("705903,692669,689603\t" + mili + "\t" + comparacoes + "\t" + movimentacoes);
+
+		escrita.abrirArquivo("matricula_mergesort.txt");
+		escrita.escrever(log); // Escreve no arquivo criado o log.
+		escrita.fecharArquivo();
+	}
+
+	public static int qtdLinhas(ArquivoTextoLeitura leitura) {
+		int qtd = 0;
+		String linhaLida = new String();
+		leitura.abrirArquivo("/tmp/players.csv");
+
+		leitura.ler(); // O cabecalho, tem que pular a primeira linha
+		linhaLida = leitura.ler();
+		while (linhaLida != null) {
+			qtd++;
+			linhaLida = leitura.ler();
+		}
+
+		leitura.fecharArquivo();
+
+		return qtd; // Retorna a quantidade de jogadores/Quantidade de linhas
+	}
+
+	public static Jogador[] preencherVetorJogador(ArquivoTextoLeitura leitura, int qtdLinhas) {
+		Jogador[] players = new Jogador[qtdLinhas]; // Reserva o armazenamento
+
+		for (int i = 0; i < qtdLinhas; i++)
+			players[i] = new Jogador(); // Cria o objeto para cada um
+
+		leitura.abrirArquivo("/tmp/players.csv");
+
+		leitura.ler(); // Remove o cabecalho
+		for (int i = 0; i < qtdLinhas; i++) {
+
+			String[] dadosDaLinha = leitura.ler().split(",", 8); // Dividir os dados da linha
+
+			// Caso necessite de remover os asterisco s� tirar o comen�rio das linhas abaixo
+			/*
+			 * String nome = dadosDaLinha[1].toString(); char ultima =
+			 * nome.charAt(nome.length()-1);
+			 * 
+			 * if(ultima=='*') { dadosDaLinha[1]=""; for(int z=0; z<nome.length()-1; z++) {
+			 * dadosDaLinha[1] += nome.charAt(z); } }
+			 */
+
+			// Seta os dados de cada linha para cada atributo do player atual
+			players[i].setId(Integer.parseInt((dadosDaLinha[0].toString()))); // Transforma array em string, para
+																				// transformar em int
+			players[i].setNome(dadosDaLinha[1].toString());
+			players[i].setAltura(Integer.parseInt((dadosDaLinha[2].toString())));
+			players[i].setPeso(Integer.parseInt((dadosDaLinha[3].toString())));
+			players[i].setUniversidade(dadosDaLinha[4].toString());
+			players[i].setAnoNascimento(Integer.parseInt((dadosDaLinha[5].toString())));
+			players[i].setCidadeNascimento(dadosDaLinha[6].toString());
+			players[i].setEstadoNascimento(dadosDaLinha[7].toString());
+
+		}
+
+		leitura.fecharArquivo();
+
+		return players;
+	}
+
+}
+
+class Merge {
+
+	/**
+	 * @param args
+	 */
+
+	private int comparacoes = 0;
+	private int movimentacoes = 0;
+
+	public Jogador[] sort(Jogador[] vetor, int n) { // Metodo para chmar ordenacao privada
+        int left = 0, rigth = n-1;
+        return method(vetor, left, rigth);
+	}
+
+	private Jogador[] method(Jogador[] vetor, int left, int rigth) { // Metodo que retorna o vetor jogadores ordenado por nome
+        if (left < rigth){
+            int mid = (rigth + left)/2;
+            method(vetor, left, mid);
+            method(vetor, mid+1, rigth);
+            merge(vetor, left, mid, rigth);
+        }
+        return vetor;
+    }
+    
+    private Jogador[] merge (Jogador[] vetor, int left, int mid, int rigth){
+
+        int i, j, k;
+        boolean is1Empty, is2Empty, isFirstLesser, areEqual, isFirstsNameLesser;
+
+        //descobrindo tamanho dos subveores
+        int n1 = mid-left+1;
+        int n2 = rigth-mid;
+
+        //criando subvetores
+        Jogador[] vet1 = new Jogador[n1];
+        Jogador[] vet2 = new Jogador[n2];
+
+        //preenchendo subvetores
+        for(i=0;i<n1;i++){
+            vet1[i] = new Jogador();
+            vet1[i] = vetor[i+left];
+        }
+        for(j=0;j<n2;j++){
+            vet2[j] = new Jogador();
+            vet2[j] = vetor[j+mid+1];
+        }
+
+        //Merge dos dois vetores
+        for (i=0,j=0,k=left; (i<n1 && j<n2 ) ; k++){
+
+            is1Empty = vet1[i].getUniversidade().trim().length() == 0;
+            is2Empty = vet2[j].getUniversidade().trim().length() == 0;
+            isFirstLesser = vet1[i].getUniversidade().compareTo(vet2[j].getUniversidade()) < 0;
+            areEqual = vet1[i].getUniversidade().compareTo(vet2[j].getUniversidade()) == 0;
+            isFirstsNameLesser = vet1[i].getNome().compareTo(vet2[j].getNome()) < 0;
+
+            if ( (!is1Empty && is2Empty) || (!is1Empty && isFirstLesser) || (areEqual && isFirstsNameLesser) )
+                vetor[k] = vet1[i++].clone();
+            else
+                vetor[k] = vet2[j++].clone();
+
+            this.comparacoes++;
+            this.movimentacoes++;
+        }
+        //Finalizar Merge
+        if ( i == n1 ){
+            for (;k<=rigth;k++)
+                vetor[k] = vet2[j++].clone();
+        }
+        else
+            for (;k<=rigth;k++)
+                vetor[k] = vet1[i++].clone();
+
+        return vetor;
+        
+    }
+
+	public int getComparacoes() {
+		return this.comparacoes;
+	}
+
+	public int getMovimentacoes() {
+		return this.movimentacoes;
+	}
+
+}
+
+class Jogador {
+
+	private int id;
+	private String nome = new String();
+	private int altura;
+	private int peso;
+	private String universidade = new String();
+	private int anoNascimento;
+	private String cidadeNascimento = new String();
+	private String estadoNascimento = new String();
+
+	public Jogador() {
+
+	}
+
+	public Jogador(int id, String nome, int altura, int peso, String universidade, int anoNascimento,
+			String cidadeNascimento, String estadoNascimento) {
+		this.id = id;
+		this.nome = nome;
+		this.altura = altura;
+		this.peso = peso;
+		this.universidade = universidade;
+		this.anoNascimento = anoNascimento;
+		this.cidadeNascimento = cidadeNascimento;
+		this.estadoNascimento = estadoNascimento;
+	}
+
+	// In�cio GETS
+	public int getId() {
+		return this.id;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public int getAltura() {
+		return this.altura;
+	}
+
+	public int getPeso() {
+		return this.peso;
+	}
+
+	public String getUniversidade() {
+		return this.universidade;
+	}
+
+	public int getAnoNascimento() {
+		return this.anoNascimento;
+	}
+
+	public String getCidadeNascimento() {
+		return this.cidadeNascimento;
+	}
+
+	public String getEstadoNascimento() {
+		return this.estadoNascimento;
+	}
+	// Fim GETS
+
+	// In�cio SETS
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setAltura(int altura) {
+		this.altura = altura;
+	}
+
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+
+	public void setUniversidade(String universidade) {
+		this.universidade = universidade;
+	}
+
+	public void setAnoNascimento(int anoNascimento) {
+		this.anoNascimento = anoNascimento;
+	}
+
+	public void setCidadeNascimento(String cidadeNascimento) {
+		this.cidadeNascimento = cidadeNascimento;
+	}
+
+	public void setEstadoNascimento(String estadoNascimento) {
+		this.estadoNascimento = estadoNascimento;
+	}
+	// Fim SETS
+
+	public Jogador clone() {
+
+		Jogador jogador = new Jogador();
+		jogador.id = this.id;
+		jogador.nome = this.nome;
+		jogador.altura = this.altura;
+		jogador.peso = this.peso;
+		jogador.universidade = this.universidade;
+		jogador.anoNascimento = this.anoNascimento;
+		jogador.cidadeNascimento = this.cidadeNascimento;
+		jogador.estadoNascimento = this.estadoNascimento;
+
+		return jogador;
+	}
+
+	public void imprimir() {
+
+		System.out.printf("[%d ## ", this.id);
+
+		System.out.printf("%s ## ", this.nome);
+
+		System.out.printf("%d ## ", this.altura);
+
+		System.out.printf("%d ## ", this.peso);
+
+		System.out.printf("%d ## ", this.anoNascimento);
+
+		if (this.universidade.trim().length() == 0) { // Se o dado esta vazio
+			System.out.printf("nao informado ## ");
+		} else {
+			System.out.printf("%s ## ", this.universidade);
+		}
+
+		if (this.cidadeNascimento.trim().length() == 0) { // Se o dado esta vazio
+			System.out.printf("nao informado ## ");
+		} else {
+			System.out.printf("%s ## ", this.cidadeNascimento);
+		}
+
+		if (this.estadoNascimento.trim().length() == 0) { // Se o dado esta vazio
+			System.out.printf("nao informado]\n");
+		} else {
+			System.out.printf("%s]\n", this.estadoNascimento);
+		}
+
+	}
+
+}
+
+class ArquivoTextoLeitura {
+
+	private BufferedReader entrada;
+
+	public void abrirArquivo(String nomeArquivo) {
+
+		try {
+			entrada = new BufferedReader(new FileReader(nomeArquivo));
+		} catch (FileNotFoundException excecao) {
+			System.out.println("Arquivo não encontrado");
+		}
+	}
+
+	public void fecharArquivo() {
+
+		try {
+			entrada.close();
+		} catch (IOException excecao) {
+			System.out.println("Erro no fechamento do arquivo de leitura: " + excecao);
+		}
+	}
+
+	public String ler() {
+
+		String textoEntrada;
+
+		try {
+			textoEntrada = entrada.readLine();
+		} catch (EOFException excecao) { // Exceção de final de arquivo.
+			return null;
+		} catch (IOException excecao) {
+			System.out.println("Erro de leitura: " + excecao);
+			return null;
+		}
+		return textoEntrada;
+	}
+}
+
+class ArquivoTextoEscrita {
+
+	private BufferedWriter saida;
+
+	public void abrirArquivo(String nomeArquivo) {
+
+		try {
+			saida = new BufferedWriter(new FileWriter(nomeArquivo));
+		} catch (FileNotFoundException excecao) {
+
+		} catch (IOException excecao) {
+
+		}
+	}
+
+	public void fecharArquivo() {
+
+		try {
+			saida.close();
+		} catch (IOException excecao) {
+
+		}
+	}
+
+	public void escrever(String textoEntrada) {
+
+		try {
+			saida.write(textoEntrada);
+			saida.newLine();
+		} catch (IOException excecao) {
+
+		}
+	}
+}
+

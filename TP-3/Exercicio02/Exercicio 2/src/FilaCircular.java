323434dea67b476e5b40a17bdfffc68fb8ea9cb7
José Maurício Guimarães França <josemauriciogf@gmail.com>
2020-11-02 22:13:00 -0300

    Update FilaCircular.java

--- TP-3/Exercicio02/Exercicio 2/src/FilaCircular.java
+++ TP-3/Exercicio02/Exercicio 2/src/FilaCircular.java
@@ -1,21 +1,73 @@
-
 public class FilaCircular {
 
-	private int tamanho;
-	private int topo;
 	private Jogador players[];
+	private int frente;
+	private int tras;
 
-}
+	FilaCircular() {
 
-public Fila() {
-	 
-}
+		frente = 0;
+		tras = 0;
+		int tamanho = 4000;
+		players = new Jogador[tamanho];
 
-	public void enfileirar (Jogador player) {
 	}
 
-Jogador desenfileirar ()
+	FilaCircular(int size) {
 
-public void mostrar()
+		frente = 0;
+		tras = 0;
+		int tamanho = size;
+		players = new Jogador[tamanho];
 
-public double obterMediaAltura ():
+	}
+
+	public void enfileirar(Jogador players) {
+
+		if (!filaCheia()) {
+			players[tras % players.lenght] = players;
+			tras++;
+		}
+
+	}
+
+	public Jogador desenfileirar() {
+
+		int retirado = -1;
+
+		if (!filaVazia()) {
+			retirado = players[frente % players.length];
+			frente++;
+		}
+
+		return retirado;
+
+	}
+
+	public boolean filaVazia() {
+
+		if (frente == tras)
+			return true;
+		else
+			return false;
+
+	}
+
+	public boolean filaCheia(){
+		
+		if ((tras + 1) % players.length) == (frente % players.lenght)
+				return true;
+		else
+			return false;
+		
+	}
+
+	public void mostrar() {
+
+	}
+
+	public double obterMediaAltura() {
+
+	}
+
+}

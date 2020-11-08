236ac752cffe5ff26201132f357cf2cdc77c3a14
José Maurício Guimarães França <josemauriciogf@gmail.com>
2020-11-08 14:32:14 -0300

    5

--- TP-3/Exercicio05/Exercicio05/src/PilhaDinamica.java
+++ TP-3/Exercicio05/Exercicio05/src/PilhaDinamica.java
@@ -1,3 +1,45 @@
 public class PilhaDinamica {
 
+	private Celula fundo; // Tipo célula e guardam o endereço de memória onde se encontra uma célula,
+							// endereço onde está o fundo da pilha
+	private Celula topo; // Endereço no topo
+
+	public PilhaDinamica() { // Criar pilha vazia, criar fundo e topo apontando p/ Sentinela
+
+		Celula sentinela = new Celula(); // Criando a nova celula
+		fundo = topo = sentinela; // Fundo e topo apontando para sentinela
+	}
+
+	public void empilhar(Jogador novo) {
+
+		Celula novaCelula = new Celula(novo); // Criando a nova celula com o item la dentro
+		novaCelula.proximo = topo;// Encadeamento com a célula anterior
+		topo = novaCelula;// Apontando o topo para a nova célula
+	}
+
+	public Jogador desempilhar() {
+
+		Celula aux; // Criar a celula auxiliar para apontar para onde o topo está, para poder mexer
+					// com o topo
+		Jogador player = null;
+		if (!pilhaVazia()) {
+
+			aux = topo;
+			topo = topo.proximo;// Apontar para a proxima celula
+			aux.proximo = null; // Tirando o lixo de memoria
+			player = aux.item;
+		}
+		return player; // Retornando o item
+	}
+
+	public boolean pilhaVazia() {// fundo e topo apontando para o mesmo lugar
+
+		boolean resp;
+		if (fundo == topo)
+			resp = true;
+		else
+			resp = false;
+
+	}
+
 }

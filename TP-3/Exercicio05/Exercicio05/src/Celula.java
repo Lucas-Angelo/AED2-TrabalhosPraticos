75e195d33b797c8cfe2df65b22c2b3f29dba21c7
José Maurício Guimarães França <josemauriciogf@gmail.com>
2020-11-08 14:33:35 -0300

    Update Celula.java

--- TP-3/Exercicio05/Exercicio05/src/Celula.java
+++ TP-3/Exercicio05/Exercicio05/src/Celula.java
@@ -3,12 +3,12 @@ public class Celula {
 	Jogador item; // Dados do jogador
 	Celula proximo; // Apontar da frente ate atras na fila
 
-	public Celula() { // Sentinela
+	public Celula() { // Sentinela, não fala o item que vai colocar na célula
 		this.item = new Jogador();
 		proximo = null;
 	}
 
-	public Celula(Jogador player) { // Preencher
+	public Celula(Jogador player) { // Preencher, o que têm que colocar na célula
 		this.item = player;
 		proximo = null;
 	}

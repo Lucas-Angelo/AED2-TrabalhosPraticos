7cb0563220a05e3f41360c6ebd491788f7f2c72f
Lucas Angelo <lcs2001_@hotmail.com>
2020-11-30 20:39:08 -0300

    Ex 1, tp4 100 verde

--- TP-4/forVerde/ex01.java
+++ TP-4/forVerde/ex01.java
@@ -1,4 +1,115 @@
-public class ABB {
+import java.io.*;
+
+public class ex01 {
+
+	/*
+	 * Considerações: 1- Deixamos comentado um código para caso for necessário
+	 * remover o asterisco do nome do jogador.
+	 */
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		ArquivoTextoLeitura leitura = new ArquivoTextoLeitura();
+
+		String idInformado = new String();
+
+		int qtdJogadores = qtdLinhas(leitura); // Procurar quantas linhas/jogadores existem para deixar mais limpo o
+												// codigo
+
+		Jogador[] players = preencherVetorJogador(leitura, qtdJogadores); // Primeiro preencher o vetor com os dados da
+																			// tabela
+
+		ABB arvore = new ABB();
+
+		do {
+			idInformado = in.readLine(); // Qual jogador deseja procurar
+
+			if (!(idInformado.equals("FIM"))) {
+
+				arvore.inserir(players[Integer.parseInt(idInformado)]);
+
+			}
+
+		} while (!(idInformado.equals("FIM")));
+
+		String nome;
+		do {
+			nome = in.readLine(); // Qual jogador deseja procurar
+
+			if (!(nome.equals("FIM"))) {
+
+				if (arvore.buscar(nome) == null)
+					System.out.print("NAO\n");
+				else
+					System.out.print("SIM\n");
+
+			}
+
+		} while (!(nome.equals("FIM")));
+
+	}
+
+	public static int qtdLinhas(ArquivoTextoLeitura leitura) {
+		int qtd = 0;
+		String linhaLida = new String();
+		leitura.abrirArquivo("/tmp/players.csv");
+
+		leitura.ler(); // O cabecalho, tem que pular a primeira linha
+		linhaLida = leitura.ler();
+		while (linhaLida != null) {
+			qtd++;
+			linhaLida = leitura.ler();
+		}
+
+		leitura.fecharArquivo();
+
+		return qtd; // Retorna a quantidade de jogadores/Quantidade de linhas
+	}
+
+	public static Jogador[] preencherVetorJogador(ArquivoTextoLeitura leitura, int qtdLinhas) {
+		Jogador[] players = new Jogador[qtdLinhas]; // Reserva o armazenamento
+
+		for (int i = 0; i < qtdLinhas; i++)
+			players[i] = new Jogador(); // Cria o objeto para cada um
+
+		leitura.abrirArquivo("/tmp/players.csv");
+
+		leitura.ler(); // Remove o cabecalho
+		for (int i = 0; i < qtdLinhas; i++) {
+
+			String[] dadosDaLinha = leitura.ler().split(",", 8); // Dividir os dados da linha
+
+			// Caso necessite de remover os asterisco só tirar o comenário das linhas abaixo
+			/*
+			 * String nome = dadosDaLinha[1].toString(); char ultima =
+			 * nome.charAt(nome.length()-1);
+			 * 
+			 * if(ultima=='*') { dadosDaLinha[1]=""; for(int z=0; z<nome.length()-1; z++) {
+			 * dadosDaLinha[1] += nome.charAt(z); } }
+			 */
+
+			// Seta os dados de cada linha para cada atributo do player atual
+			players[i].setId(Integer.parseInt((dadosDaLinha[0].toString()))); // Transforma array em string, para
+																				// transformar em int
+			players[i].setNome(dadosDaLinha[1].toString());
+			players[i].setAltura(Integer.parseInt((dadosDaLinha[2].toString())));
+			players[i].setPeso(Integer.parseInt((dadosDaLinha[3].toString())));
+			players[i].setUniversidade(dadosDaLinha[4].toString());
+			players[i].setAnoNascimento(Integer.parseInt((dadosDaLinha[5].toString())));
+			players[i].setCidadeNascimento(dadosDaLinha[6].toString());
+			players[i].setEstadoNascimento(dadosDaLinha[7].toString());
+
+		}
+
+		leitura.fecharArquivo();
+
+		return players;
+	}
+
+}
+
+class ABB {
 
 	private NodoJogador raiz; // referência à raiz da árvore.
 
@@ -207,7 +318,7 @@ public class ABB {
 		imprimirEmOrdem(raiz);
 	}
 
-	private void imprimirEmOrdem(NodoJogador raizArvore) { // Caso queira imprirmir decrescente s� trocar o .esquerda
+	private void imprimirEmOrdem(NodoJogador raizArvore) { // Caso queira imprirmir decrescente s󠴲ocar o .esquerda
 
 		if (raizArvore != null) { // Verificar nao o no de agora nao esta nulo
 			imprimirEmOrdem(raizArvore.esquerda);
@@ -285,4 +396,200 @@ public class ABB {
 			return 1 + contarNumJogadores(raizArvore.esquerda) + contarNumJogadores(raizArvore.direita);
 	}
 
+}
+
+class NodoJogador {
+
+	Jogador item; // contem os dados do jogador armazenado no nodo da arvore.
+	NodoJogador direita; // referencia ao nodo armazenado, na arvore, a direita do jogador em questao.
+	NodoJogador esquerda; // referencia ao nodo armazenado, na arvore, a esquerda do jogador em questao.
+
+	public NodoJogador(Jogador registro) {
+		item = registro;
+		direita = null;
+		esquerda = null;
+	}
+
+}
+
+class Jogador {
+
+	private int id;
+	private String nome = new String();
+	private int altura;
+	private int peso;
+	private String universidade = new String();
+	private int anoNascimento;
+	private String cidadeNascimento = new String();
+	private String estadoNascimento = new String();
+
+	public Jogador() {
+
+	}
+
+	public Jogador(int id, String nome, int altura, int peso, String universidade, int anoNascimento,
+			String cidadeNascimento, String estadoNascimento) {
+		this.id = id;
+		this.nome = nome;
+		this.altura = altura;
+		this.peso = peso;
+		this.universidade = universidade;
+		this.anoNascimento = anoNascimento;
+		this.cidadeNascimento = cidadeNascimento;
+		this.estadoNascimento = estadoNascimento;
+	}
+
+	// Início GETS
+	public int getId() {
+		return this.id;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public int getAltura() {
+		return this.altura;
+	}
+
+	public int getPeso() {
+		return this.peso;
+	}
+
+	public String getUniversidade() {
+		return this.universidade;
+	}
+
+	public int getAnoNascimento() {
+		return this.anoNascimento;
+	}
+
+	public String getCidadeNascimento() {
+		return this.cidadeNascimento;
+	}
+
+	public String getEstadoNascimento() {
+		return this.estadoNascimento;
+	}
+	// Fim GETS
+
+	// Início SETS
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setAltura(int altura) {
+		this.altura = altura;
+	}
+
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+
+	public void setUniversidade(String universidade) {
+		this.universidade = universidade;
+	}
+
+	public void setAnoNascimento(int anoNascimento) {
+		this.anoNascimento = anoNascimento;
+	}
+
+	public void setCidadeNascimento(String cidadeNascimento) {
+		this.cidadeNascimento = cidadeNascimento;
+	}
+
+	public void setEstadoNascimento(String estadoNascimento) {
+		this.estadoNascimento = estadoNascimento;
+	}
+	// Fim SETS
+
+	public Jogador clone() {
+
+		Jogador jogador = new Jogador();
+		jogador.id = this.id;
+		jogador.nome = this.nome;
+		jogador.altura = this.altura;
+		jogador.peso = this.peso;
+		jogador.universidade = this.universidade;
+		jogador.anoNascimento = this.anoNascimento;
+		jogador.cidadeNascimento = this.cidadeNascimento;
+		jogador.estadoNascimento = this.estadoNascimento;
+
+		return jogador;
+	}
+
+	public void imprimir() {
+
+		System.out.printf("[%d ## ", this.id);
+
+		System.out.printf("%s ## ", this.nome);
+
+		System.out.printf("%d ## ", this.altura);
+
+		System.out.printf("%d ## ", this.peso);
+
+		System.out.printf("%d ## ", this.anoNascimento);
+
+		if (this.universidade.trim().length() == 0) { // Se o dado esta vazio
+			System.out.printf("nao informado ## ");
+		} else {
+			System.out.printf("%s ## ", this.universidade);
+		}
+
+		if (this.cidadeNascimento.trim().length() == 0) { // Se o dado esta vazio
+			System.out.printf("nao informado ## ");
+		} else {
+			System.out.printf("%s ## ", this.cidadeNascimento);
+		}
+
+		if (this.estadoNascimento.trim().length() == 0) { // Se o dado esta vazio
+			System.out.printf("nao informado]\n");
+		} else {
+			System.out.printf("%s]\n", this.estadoNascimento);
+		}
+
+	}
+
+}
+
+class ArquivoTextoLeitura {
+
+	private BufferedReader entrada;
+
+	public void abrirArquivo(String nomeArquivo) {
+
+		try {
+			entrada = new BufferedReader(new FileReader(nomeArquivo));
+		} catch (FileNotFoundException excecao) {
+			System.out.println("Arquivo não encontrado");
+		}
+	}
+
+	public void fecharArquivo() {
+
+		try {
+			entrada.close();
+		} catch (IOException excecao) {
+			System.out.println("Erro no fechamento do arquivo de leitura: " + excecao);
+		}
+	}
+
+	public String ler() {
+
+		String textoEntrada;
+
+		try {
+			textoEntrada = entrada.readLine();
+		} catch (EOFException excecao) { // Exceção de final de arquivo.
+			return null;
+		} catch (IOException excecao) {
+			System.out.println("Erro de leitura: " + excecao);
+			return null;
+		}
+		return textoEntrada;
+	}
 }
